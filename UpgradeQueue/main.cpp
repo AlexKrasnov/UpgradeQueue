4614be50fdf1a49f7d939c25d2c0b200be126a20
AlexKrasnov <ya.sasha-krasnov@yandex.ru>
2017-01-13 12:30:04 +0300

    main

--- UpgradeQueue/main.cpp
+++ UpgradeQueue/main.cpp
@@ -1,4 +1,30 @@
+#include "UpgradeQueue.cpp"
+
 void main()
 {
+	try
+	{
+		setlocale (LC_ALL, "Rus");
+		cout << "\t***Демонстрация работы стека и очереди с поддержкой минимума***\n\n";
 
+		UpgradeStack<int> s;
+		for (int i=0;i<5;i++)
+			s.push(int(pow(-1,i))*i);
+		cout << "Стек: ";
+		s.print();
+		cout << "Min элемент в стеке: " << s.FindMin() << endl;
+
+		cout << endl;
+
+		UpgradeQueue<int> q;
+		for (int i=0;i<5;i++)
+			q.push(int(pow(-1,i))*i);
+		cout << "Очередь: ";
+		q.print();
+		cout << "Min элемент в очереди: " << q.FindMin() << endl;
+	}
+	catch (const char* error)
+	{
+		cout << error << endl;
+	}
 }
